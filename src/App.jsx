2fa2c4b59309b35de594bbe64bfd1c6241eb4100
burhanudinrabbani666 <burhanudinrabbani666@gmail.com>
2026-02-03 20:37:20 +0700

    feat: using lazy react function to getting better performance

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,67 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
 
-import Product from "./pages/Product";
-import Pricing from "./pages/Pricing";
-import Homepage from "./pages/Homepage";
-import PageNotFound from "./pages/PageNotFound";
-import AppLayout from "./pages/AppLayout";
-import Login from "./pages/Login";
+import { CitiesProvider } from "./contexts/CitiesProvider";
+import { AuthProvider } from "./contexts/fakeAuth/fakeAuthContext";
+import ProtectedRoute from "./pages/ProtectedRoute";
+
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
-import { CitiesProvider } from "./contexts/CitiesProvider";
-import { AuthProvider } from "./contexts/fakeAuth/fakeAuthContext";
-import ProtectedRoute from "./pages/ProtectedRoute";
+import SpinnerFullPage from "./components/SpinnerFullPage";
+
+// import Product from "./pages/Product";
+// import Pricing from "./pages/Pricing";
+// import Homepage from "./pages/Homepage";
+// import PageNotFound from "./pages/PageNotFound";
+// import AppLayout from "./pages/AppLayout";
+// import Login from "./pages/Login";
+
+/*
+dist/assets/index-C-HNJziR.css   31.33 kB │ gzip:   5.17 kB
+dist/assets/index-Dm-soLZY.js   580.57 kB │ gzip: 171.27 kB
+*/
+
+const Homepage = lazy(() => import("./pages/Homepage"));
+const Product = lazy(() => import("./pages/Product"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const PageNotFound = lazy(() => import("./pages/PageNotFound"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
+const Login = lazy(() => import("./pages/Login"));
 
 function App() {
   return (
     <AuthProvider>
       <CitiesProvider>
         <BrowserRouter>
-          <Routes>
-            <Route index element={<Homepage />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="product" element={<Product />} />
-            <Route path="login" element={<Login />} />
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              {/* Redirect to app/cities */}
-              <Route index element={<Navigate replace to="cities" />} />
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route index element={<Homepage />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                {/* Redirect to app/cities */}
+                <Route index element={<Navigate replace to="cities" />} />
 
-              <Route path="cities/:id" element={<City />} />
-              <Route path="cities" element={<CityList />} />
-              <Route path="country" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
+                <Route path="cities/:id" element={<City />} />
+                <Route path="cities" element={<CityList />} />
+                <Route path="country" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
 
-            {/* Executed when nothing oath matched */}
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
+              {/* Executed when nothing oath matched */}
+              <Route path="*" element={<PageNotFound />} />
+            </Routes>
+          </Suspense>
         </BrowserRouter>
       </CitiesProvider>
     </AuthProvider>
